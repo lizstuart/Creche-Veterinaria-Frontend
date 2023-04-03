5fa4f36c0382954945542a5bd44b3afa22e5fdeb
Lizstuart <lizstuart.nut.ufmg@gmail.com>
2023-04-03 19:37:52 -0300

    Estruturação da página Home

--- vet-app/src/pages/Home/index.js
+++ vet-app/src/pages/Home/index.js
@@ -1,8 +1,82 @@
+import "./styles.css";
+import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
+
 function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="container__home">
+      <Header />
+      <main className="section__home">
+        <div className="main__form">
+          <form className="form__home">
+            <h1>Bem-vindo(a)</h1>
+            <label className="label__nome__home" htmlFor="nome">
+              Nome
+            </label>
+            <input
+              className="input__nome__home"
+              type="text"
+              name="nome"
+              // value={}
+            />
+            <label className="label__especie__home" htmlFor="especie">
+              Espécie
+            </label>
+            <input
+              className="input__especie__home"
+              type="text"
+              name="especie"
+              // value={}
+            />
+            <label className="label__raça__home" htmlFor="raça">
+              Raça
+            </label>
+            <input
+              className="input__raça__home"
+              type="text"
+              name="raça"
+              // value={}
+            />
+            <label className="label__gênero__home" htmlFor="gênero">
+              Gênero
+            </label>
+            <input
+              className="input__gênero__home"
+              type="text"
+              name="gênero"
+              // value={}
+            />
+            <label className="label__idade__home" htmlFor="idade">
+              Idade
+            </label>
+            <input
+              className="input__idade__home"
+              type="number"
+              name="idade"
+              // value={}
+            />
+            <button className="btn__home">Confirmar</button>
+            <span className="">Não possui cadastro?</span>
+          </form>
+        </div>
+        <div className="table">
+          <h1>Quem somos?</h1>
+          <p>
+            Somos uma família apaixonada por animais. Adquirimos um racho em
+            2019 e iniciamos nosso projeto de creche de pet.
+          </p>
+          <h1>Onde estamos?</h1>
+          <p>Estamos em Belo Horizonte/MG, na região Norte da cidade.</p>
+          <h1>Nossa missão</h1>
+          <p>
+            Proporcionar um local seguro e aconchegante aos pets e confiável a
+            seus tutotes. Nosso objetivo é oferecer um ambiente acolhedor e
+            divertido!
+          </p>
+        </div>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- vet-app/src/routes.js
+++ vet-app/src/routes.js
@@ -1,6 +1,6 @@
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 // import { getItem } from "./Utils/storage";
 
@@ -16,8 +16,8 @@ function MainRoutes() {
       <Route path="/" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
 
+      <Route path="/home" element={<Home />} />
       {/* <Route element={<ProtectedRoutes redirectTo="/" />}>
-        <Route path="/home" element={<Home />} />
       </Route> */}
     </Routes>
   );
